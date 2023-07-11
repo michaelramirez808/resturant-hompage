05f973c40f48521dd8c44b97687c9802cf90a228
Michael <michael76ramirez@gmail.com>
2023-07-11 15:13:56 -0700

    added menu button creation function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-function pageLoad(){
+export function pageLoad(){
+
+    //create header
     const content = document.getElementById('content');
-    const header = document.createElement('h1');
-    header.textContent = 'Resturant Homepage';
+    const header = document.createElement('h2');
+    header.classList.add('header')
     content.appendChild(header);
+
+    //create menu buttons
+    const headerButtons = [];
+    for(let i = 0; i < 3; i++){
+        const createButton = document.createElement('button');
+        createButton.textContent = `Button ${i}`;
+        createButton.setAttribute('button-id', i);
+        headerButtons.push(createButton);
+        header.appendChild(createButton);
+    }
+        const targetButtonId = 0;
+        const targetButton = headerButtons.find(createButton => createButton.getAttribute('button-id') === `${targetButtonId}`);
+        if(targetButton){
+            targetButton.textContent = 'Hello';
+        }
 }
 
 pageLoad();
