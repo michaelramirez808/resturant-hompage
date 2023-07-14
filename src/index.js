0cff943cd482def2ebe2430a7c21f108220df30d
Michael <michael76ramirez@gmail.com>
2023-07-14 16:01:26 -0700

    added 3 header buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 export function pageLoad(){
 
-    //create header
     const content = document.getElementById('content');
-    const header = document.createElement('h2');
-    header.classList.add('header')
+
+    //create header
+    const header = document.createElement('div');
+    header.classList.add('header');
     content.appendChild(header);
+
+    //create title
+    const title = document.createElement('h2');
+    title.textContent = 'Resturant Homepage';
+    title.classList.add('title');
+    header.appendChild(title);
+
+    //body elements
+    const bodyContent = document.createElement('div');
+    bodyContent.textContent = 'Lorem Ipsum';
+    bodyContent.classList.add('bodyContent');
+    content.appendChild(bodyContent);
+    
 
     //create menu buttons
     const headerButtons = [];
@@ -15,10 +29,20 @@ export function pageLoad(){
         headerButtons.push(createButton);
         header.appendChild(createButton);
     }
-        const targetButtonId = 0;
-        const targetButton = headerButtons.find(createButton => createButton.getAttribute('button-id') === `${targetButtonId}`);
-        if(targetButton){
-            targetButton.textContent = 'Hello';
+        let targetButtonId0 = 0;
+        const targetButton0 = headerButtons.find(createButton => createButton.getAttribute('button-id') === `${targetButtonId0}`);
+        if(targetButton0){
+            targetButton0.textContent = 'Welcome';
+        }
+        let targetButtonId1 = 1;
+        const targetButton1 = headerButtons.find(createButton => createButton.getAttribute('button-id') === `${targetButtonId1}`);
+        if(targetButton1){
+            targetButton1.textContent = 'Menu';
+        }
+        let targetButtonId2 = 2;
+        const targetButton2 = headerButtons.find(createButton => createButton.getAttribute('button-id') === `${targetButtonId2}`);
+        if(targetButton2){
+            targetButton2.textContent = 'Contact';
         }
 }
 
